b476767470d575f339aaec952a47cd56aaa47f27
Harmedino <adamsahmed191@gmail.com>
2022-09-09 07:06:32 +0100

    Angular Todo List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myapp';
+
+  message = '';
+  array: any = [];
+  add() {
+    this.array.push({ name: this.message, done: false });
+  }
+  del(i: any) {
+    this.array.splice(i, 1);
+  }
+
+  check(i: any) {
+    this.array[i].done
+      ? (this.array[i].done = false)
+      : (this.array[i].done = true);
+  }
 }
